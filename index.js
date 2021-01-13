28acd97a8c5a6dbfb527c630c1a724808f06fd74
fsrocha-dev <contato.frankrocha@gmail.com>
2021-01-13 15:46:19 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const exect = require('child_process').exec;
+const progressBar = require('./progressBar');
+const path = require('path');
+const fs = require('fs');
+
+const titanic = function(receivedSoundPath, receivedSoundTime) {
+	const mainPath = path.dirname(fs.realpathSync(__filename));
+	const soundPath = receivedSoundPath ? receivedSoundPath : path.join(mainPath, './titanic-fails');
+	const soundTime = receivedSoundTime ? receivedSoundTime : 38;
+
+	const linuxcmd = 'paplay ' + soundPath + '.ogg';
+	const windowscmd = path.join(mainPath, './forWindows.vbs') + ' ' + soundPath + '.mp3';
+	const maccmd = 'afplay ' + soundPath + '.mp3';
+
+	const platform = process.platform;
+
+	console.log('titanic-fails ;-;');
+
+	switch (platform) {
+		case 'linux':
+			exec(linuxcmd);
+			break;
+		case 'win32':
+			exec(windowscmd);
+			break;
+		case 'darwin':
+			exec(maccmd);
+			break;
+	}
+	progressBar(soundTime);
+
+	function exec(cmd) {
+		return exect(cmd, function(error, stdout, stderr) {
+			if (error) console.error(error);
+		});
+	}
+};
+
+module.exports = titanic;
+
+if (!module.parent) {
+	titanic();
+}
