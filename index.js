4d00e37c3022a6be589135bb68f338049a57d2b8
fsrocha-dev <contato.frankrocha@gmail.com>
2021-01-13 16:32:46 -0300

    removed progressbar

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
+#!/usr/bin/env node
+
 const exect = require('child_process').exec;
-const progressBar = require('./progressBar');
 const path = require('path');
 const fs = require('fs');
 
-const titafail = function(receivedSoundPath, receivedSoundTime) {
-	const mainPath = path.dirname(fs.realpathSync(__filename));
-	const soundPath = receivedSoundPath ? receivedSoundPath : path.join(mainPath, './titafail');
-	const soundTime = receivedSoundTime ? receivedSoundTime : 38;
+const mainPath = path.dirname(fs.realpathSync(__filename));
+const soundPath = path.join(mainPath, './titafail');
 
+const titafail = function() {
 	const linuxcmd = 'paplay ' + soundPath + '.ogg';
 	const windowscmd = path.join(mainPath, './forWindows.vbs') + ' ' + soundPath + '.mp3';
 	const maccmd = 'afplay ' + soundPath + '.mp3';
 
 	const platform = process.platform;
 
-	console.log('titafail ;-;');
-
-	switch (platform) {
-		case 'linux':
-			exec(linuxcmd);
-			break;
-		case 'win32':
-			exec(windowscmd);
-			break;
-		case 'darwin':
-			exec(maccmd);
-			break;
+	if (platform === 'linux') {
+		return exec(linuxcmd);
+	} else if (platform === 'win32') {
+		return exec(windowscmd);
+	} else if (platform === 'darwin') {
+		return exec(maccmd);
 	}
-	progressBar(soundTime);
 
 	function exec(cmd) {
 		return exect(cmd, function(error, stdout, stderr) {
